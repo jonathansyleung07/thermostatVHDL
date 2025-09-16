7acf950942a4222a56054394dbf34a744b4705e8
Jonathan Leung <106103630+jonathansyleung07@users.noreply.github.com>
2025-09-15 21:21:48 -0400

    Add files via upload

---
+++ test_tempmux.vhd
@@ -0,0 +1,77 @@
+entity test_tempmux is
+
+end test_tempmux;
+
+architecture BEHAV of test_tempmux is 
+
+component therm 
+    port (Current_Temp   : in bit_vector(6 downto 0);
+
+          Desired_Temp   : in bit_vector(6 downto 0);
+
+          Display_Select : in bit;
+
+          HEAT           : in bit;
+       
+          COOL           : in bit;
+
+          A_C_ON         : out bit;
+
+          FURNACE_ON     : out bit;
+
+          Temp_Display   : out bit_vector(6 downto 0));
+       
+ end component; 
+ 
+ signal Current_Temp, Desired_Temp : bit_vector(6 downto 0);
+ signal Display_Select, HEAT, COOL : bit; 
+ signal A_C_ON, FURNACE_ON : bit;
+ signal Temp_Display : bit_vector(6 downto 0);
+ 
+ begin 
+ 
+ UUT: therm port map ( Current_Temp => Current_Temp,
+                       Desired_Temp => Desired_Temp,
+                       Display_Select => Display_Select,
+                       HEAT => HEAT,
+                       COOL => COOL, 
+                       A_C_ON => A_C_ON,
+                       FURNACE_ON => FURNACE_ON,
+                       Temp_Display => Temp_Display);
+                       
+ process 
+ begin 
+ --Test if Display works
+ Current_Temp <= "0011111"; --32 decimal
+ Desired_Temp <= "0001111"; --15 binary
+ Display_Select <= '0'; --Should display desired temp
+ wait for 10ns; 
+ Display_Select <= '1'; -- Should diplay current temp
+ wait for 10ns;
+ --Testing if too hot (31) and want to lower temp (15) using AC
+ COOL <= '1'; --The logic should turn on A_C_ON (set it to high) because Current_Temp>Desired Temp
+ wait for 10ns;
+ --Testing if furnace will not activate because current temp > desired temp
+ COOL <= '0'; --turning off AC
+ wait for 10ns;
+ HEAT <= '1';
+ wait for 10ns;
+ HEAT <= '0'; --turning off furnace
+ --Switching current temp and desired temp to test if Furnace works
+ Current_Temp <= "0000001"; --1 decimal
+ Desired_Temp <= "0001111"; --15 binary
+ wait for 10ns;
+ HEAT <= '1'; -- furnace should turn on because Current Temp < Desired_Temp
+ wait for 10ns;
+ HEAT <= '0';
+ wait for 10ns;
+ --Testing if AC will not activate because current temp < desired temp
+ COOL <= '1';
+ wait;
+ 
+ end process;
+ 
+ end BEHAV;
+ 
+                       
+
---
+++ therm_mux.vhd
@@ -0,0 +1,57 @@
+entity therm is
+
+port ( Current_Temp   : in bit_vector(6 downto 0);
+
+       Desired_Temp   : in bit_vector(6 downto 0);
+
+       Display_Select : in bit; --Switch for choosing to display either desired or current temp
+
+       HEAT           : in bit; --Switch for turning furnace on
+       
+       COOL           : in bit; --Switch for turning AC on
+
+       A_C_ON         : out bit;
+
+       FURNACE_ON     : out bit;
+
+       Temp_Display   : out bit_vector(6 downto 0)); --Used to display chosen temp 
+
+       
+
+end therm;
+
+
+architecture BEHAV of therm is
+
+begin
+
+
+process (Current_Temp, Desired_Temp, Display_Select, COOL, HEAT)
+
+begin
+
+    if Display_Select = '1' then
+
+        Temp_Display <= Current_Temp;
+
+    else
+
+        Temp_Display <= Desired_Temp;
+
+    end if;
+    
+    if COOL = '1' and Current_Temp > Desired_Temp  then 
+            A_C_ON <= '1';
+       else 
+            A_C_ON <= '0';
+    end if;
+    
+    if HEAT = '1' and Current_Temp < Desired_Temp then 
+            FURNACE_ON <= '1';
+       else 
+            FURNACE_ON <= '0';
+    end if;
+                      
+end process;
+
+end BEHAV;
