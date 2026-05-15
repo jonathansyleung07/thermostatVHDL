28ba18898a300c95499829d7301bd420d9ef5a9b
Jonathan Leung <106103630+jonathansyleung07@users.noreply.github.com>
2026-05-14 18:38:21 -0700

    Updated thermo.vhd
    Added assertions for each applied stimulus. Fixed a few other minor errors.

--- test_thermo.vhd
+++ test_thermo.vhd
@@ -38,22 +38,47 @@ begin
     process
     begin
         --Test if Display logic behaves correctly
-        Current_Temp   <= "0011111"; --32 decimal
+        Current_Temp   <= "0011111"; --31 decimal
         Desired_Temp   <= "0001111"; --15 decimal
         Display_Select <= '0'; --Should display desired temp
         wait for 10 ns;
 
+        assert Temp_Display = Desired_Temp
+            report "FAIL: Display_Select=0 should show Desired_Temp"
+            severity error;
+
         Display_Select <= '1'; -- Should diplay current temp
         wait for 10 ns;
+            
+        assert Temp_Display = Current_Temp
+            report "FAIL: Display_Select=1 should show Current_Temp"
+            severity error;
+
         --Testing if AC logic behaves correctly, COOL is set to high
         COOL <= '1'; --The logic should set A_C_ON to high because Current_Temp > Desired Temp
         wait for 10 ns;
 
+        assert A_C_ON = '1'
+            report "FAIL: AC should be ON when COOL=1 and Current>Desired"
+            severity error;
+        assert FURNACE_ON = '0'
+            report "FAIL: Furnace should be OFF when AC is active"
+            severity error;
+
         COOL <= '0'; 
         wait for 10 ns;
+            
+        assert A_C_ON = '0'
+            report "FAIL: AC should be OFF when COOL=0"
+            severity error;
+
         --Testing if furnace logic behaves correctly (if Current_Temp > Desired Temp furnace should not turn on)
         HEAT <= '1';
         wait for 10 ns;
+            
+        assert FURNACE_ON = '0'
+            report "FAIL: Furnace should be OFF when Current>Desired"
+            severity error;
 
         HEAT <= '0';
          --Switching current temp and desired temp for testing more cases
@@ -64,10 +89,20 @@ begin
         HEAT <= '1';
         wait for 10 ns;
 
+        assert FURNACE_ON = '1'
+            report "FAIL: Furnace should be ON when HEAT=1 and Current<<Desired"
+            severity error;
+
+        -- Test AC OFF when Current < Desired (COOL=1 but 1 < 15)
         HEAT <= '0';
-        wait for 10 ns;
-        --Testing if AC logic behaves correctly (if Current_Temp < Desired Temp AC should not turn on)
         COOL <= '1'; 
+        wait for 10 ns;
+
+        assert A_C_ON = '0'
+            report "FAIL: AC should be OFF when Current<<Desired"
+            severity error;
+
+        report "Testbench completed"; 
         wait;
     end process;
 
